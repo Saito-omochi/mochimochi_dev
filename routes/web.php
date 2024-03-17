217ec6f78457589ef933fa5ee479f717374335df
Saito-omochi <saito3cat@gmail.com>
2024-03-17 11:39:05 +0900

    mochi_3_commit

--- routes/web.php
+++ routes/web.php
@@ -29,14 +29,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/', [PostController::class, 'index']);
+    Route::post('/posts',  [PostController::class, 'store']);
+    Route::get('/posts/create',  [PostController::class, 'create']);
+    Route::get('/posts/{post}',  [PostController::class, 'show']);
+    Route::put('/posts/{post}',  [PostController::class, 'update']);
+    Route::delete('/posts/{post}',  [PostController::class, 'delete']);
+    Route::get('/posts/{post}/edit',  [PostController::class, 'edit']);
+    Route::get('/categories/{category}', [CategoryController::class,'index']);
+    Route::get('/prefectures/{prefecture}', [PrefectureController::class,'index']);
 });
 
-Route::get('/', [PostController::class, 'index']);
-Route::post('/posts',  [PostController::class, 'store']);
-Route::get('/posts/create',  [PostController::class, 'create']);
-Route::get('/posts/{post}',  [PostController::class, 'show']);
-Route::put('/posts/{post}',  [PostController::class, 'update']);
-Route::delete('/posts/{post}',  [PostController::class, 'delete']);
-Route::get('/posts/{post}/edit',  [PostController::class, 'edit']);
-Route::get('/categories/{category}', [CategoryController::class,'index']);
-Route::get('/prefectures/{prefecture}', [PrefectureController::class,'index']);
+require __DIR__.'/auth.php';
