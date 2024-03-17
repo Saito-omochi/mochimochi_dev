1483f887c46fb993bb87e151386f5bc3a20beb6e
suzume715 <suzume2eg@gmail.com>
2024-03-17 17:44:09 +0900

    show.blade.php変更

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,58 +12,78 @@
         <div class="title">
             <h1>{{ $post->title }}</h1>
         </div>
-            <p>
-                <img src="{{asset('storage/' . $post->image)}}" width="500px" height="auto" />
-                <img src="{{asset('storage/' . $post->image2)}}" width="500px" height="auto" />
-            </p>
-            <p>本文：{{ $post->body }}</p>
+        
+        <div class="edit">
+            <p class="edit">[<a href="/posts/{{ $post->id }}/edit">編集</a>]</p>
+        </div>
+        
+        </div class="delete">
+            <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                @csrf
+                @method('DELETE')
+                 <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+            </form>
+        </div>    
+        
+        <div class="images">
+            <hr>
+            <img src="{{asset('storage/' . $post->image)}}" width="500px" height="auto" />
+            <img src="{{asset('storage/' . $post->image2)}}" width="500px" height="auto" />
+            <hr>
+        </div>  
+        
+        <div class="information">
+            <p>{{ $post->body }}</p>
             <p>都道府県：
                 <a href="/prefectures/{{ $post->prefecture->id }}">{{ $post->prefecture->name }}</a>
             </p>
             <p>住所：{{ $post->address }}</p>
-            <p>桜の種類：
+            <p>見られる桜：
                 @foreach($post->categories as $category)   
                     <a href="/categories/{{ $category->id }}">{{ $category->name }}</a>
                 @endforeach
             </p>
         </div>
-        <div>
-            <p>コメント：</p>
+        
+        <div class="comment_title" style='padding-top: 15px'>
+            <h2>コメント</h2>
+        </div>
+        
+        <div class="comment_space">
+        
+        <div class="comment" style='border:solid 1px; margin-bottom: 0px; padding-left: 15px'>    
             @foreach($post->comments as $comment)  
                 <p>{{ $comment->comment }}</p>
             @endforeach
         </div>
-        <div>
-            <p class="edit">[<a href="/posts/{{ $post->id }}/edit">編集</a>]</p>
-            <a href="/">戻る</a>
+        
+        <div class="to comment" style='padding-left: 15px'>
+            <form action="/make_comment" method="POST">
+                @csrf
+                <div>
+                    <h4>コメントする</h4>
+                    <input type='hidden' name='comment[post_id]' value="{{ $post->id }}">
+                    <input type="text" name="comment[comment]" placeholder="コメントを入力" value="{{ old('comment.comment') }}"/>
+                    <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
+                </div>
+                <input type="submit" value="送信"/>
+            </form>
         </div>
         
-        <form action="/make_comment" method="POST">
-            @csrf
-            <div>
-                <h2>コメント</h2>
-                <input type='hidden' name='comment[post_id]' value="{{ $post->id }}">
-                <input type="text" name="comment[comment]" placeholder="タイトル" value="{{ old('comment.comment') }}"/>
-                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
-            </div>
-            <input type="submit" value="送信"/>
-        </form>
+        </div>
         
-        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-            @csrf
-            @method('DELETE')
-            <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
-        </form>
-        
-        <span class="text-red-100">text-red-100</span><br>
+        <div class="back" style='padding-top: 25px'>
+            <a href="/">一覧へ戻る</a>
+        </div>
     </body>
+    
     <script>
-    function deletePost(id) {
-        'use strict'
+        function deletePost(id) {
+            'use strict'
 
-        if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
-            document.getElementById(`form_${id}`).submit();
+            if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                document.getElementById(`form_${id}`).submit();
+            }
         }
-    }
-</script>
+    </script>
 </html>
